05fc901727062f93138e72a95759cdb56c73db59
Erdij <yildirimsukruerdi@gmail.com>
2022-06-24 02:06:03 +0300

    comp_Lifecycle

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -33,12 +33,17 @@ class Collapse extends React.Component {
     return (
       <div>
         <button className="btn btn-primary w-100" onClick={this.showMore}>
-          Link with href
+          {/* {this.props.children.props.cardTitle} */}
+          {React.Children.map(
+            this.props.children,
+            (children) => children.props.cardTitle
+          )}
         </button>
         {this.state.showContent ? (
           <div className="collapse show ">
-            {this.props.children}
+            {/* {this.props.children} */}
             {/* {this.state.showContent} */}
+            {React.Children.map(this.props.children, (children) => children)}
           </div>
         ) : null}
       </div>
